b69a6961e2ae0e1715289fe302d93eba38253e3a
MisakiDev <thatmisakidev@outlook.com>
2026-03-10 14:56:34 -0400

    feat: implement load + bind + drop modules

--- crates/compiler-core/src/lib.rs
+++ crates/compiler-core/src/lib.rs
@@ -1,4 +1,31 @@
-/// Returns a simple message.
-pub const fn greet() -> &'static str {
-    "Hello, World!"
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Compiler {
+    pub modules: HashMap<String, String>,
+}
+
+impl Compiler {
+    pub fn drop_module(&mut self, name: impl AsRef<str>) -> Option<()> {
+        if self.modules.contains_key(name.as_ref()) {
+            self.modules.remove(name.as_ref());
+            return Some(());
+        }
+        None
+    }
+
+    pub fn load_module(&mut self, name: impl AsRef<str>, content: impl AsRef<str>) -> Option<()> {
+        if self.modules.contains_key(name.as_ref()) {
+            return None;
+        }
+        self.modules
+            .insert(name.as_ref().into(), content.as_ref().into());
+        Some(())
+    }
+
+    pub fn bind_module(&mut self, name: impl AsRef<str>, content: impl AsRef<str>) -> Option<()> {
+        self.modules.get_mut(name.as_ref()).map(|entry| {
+            *entry = content.as_ref().into();
+        })
+    }
 }
--- crates/compiler-ffi/src/lib.rs
+++ crates/compiler-ffi/src/lib.rs
@@ -1,7 +1,56 @@
-use std::ffi::{CString, c_char};
+#![allow(clippy::missing_safety_doc)]
 
-/// Returns a simple message.
-#[unsafe(no_mangle)]
-pub extern "C" fn greet() -> *const c_char {
-    CString::new(compiler_core::greet()).unwrap().into_raw()
+use std::ffi::{CStr, c_char};
+
+pub struct CompilerHandle(pub *mut compiler_core::Compiler);
+
+impl CompilerHandle {
+    #[unsafe(no_mangle)]
+    pub unsafe extern "C" fn drop_module(&mut self, name: *const c_char) -> bool {
+        unsafe {
+            self.0
+                .as_mut()
+                .and_then(|compiler| {
+                    let name = CStr::from_ptr(name).to_str().ok()?;
+                    compiler.drop_module(name)
+                })
+                .is_some()
+        }
+    }
+
+    #[unsafe(no_mangle)]
+    pub unsafe extern "C" fn load_module(
+        &mut self,
+        name: *const c_char,
+        content: *const c_char,
+    ) -> bool {
+        unsafe {
+            self.0
+                .as_mut()
+                .and_then(|compiler| {
+                    let name = CStr::from_ptr(name).to_str().ok()?;
+                    let content = CStr::from_ptr(content).to_str().ok()?;
+                    compiler.load_module(name, content)
+                })
+                .is_some()
+        }
+    }
+
+    #[unsafe(no_mangle)]
+    pub unsafe extern "C" fn bind_module(
+        &mut self,
+        name: *const c_char,
+        content: *const c_char,
+    ) -> bool {
+        unsafe {
+            self.0
+                .as_mut()
+                .and_then(|compiler| {
+                    let name = CStr::from_ptr(name).to_str().ok()?;
+                    let content = CStr::from_ptr(content).to_str().ok()?;
+                    compiler.bind_module(name, content)
+                })
+                .is_some()
+        }
+    }
 }
